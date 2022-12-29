89f121cc09811902c94a63ff94fdcea747209560
Kiberlexx <kiberlexx@mail.ru>
2022-12-29 12:20:58 +0800

    исправил задачу №1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,12 +1,12 @@
 ﻿int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
-    if (arg2 > result) {result = arg2;}
-    if (arg3 > result) {result = arg3;}
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
     return result;
 }
-int[] Array = { 1, 2, 3, 4, 5, 6 };
-int sum = Max(Max(Array[0], Array[1], Array[2]), Max(Array[3], Array[4], Array[5]));
+int[] Array = { 12, 42, 315, 34, 85, 6, 9, 80, 79 };
+int sum = Max(Max(Array[0], Array[1], Array[2]), Max(Array[3], Array[4], Array[5]), Max(Array[6], Array[7], Array[8]));
 
 
 Console.WriteLine(sum);
